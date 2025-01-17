7a0c0133219b7f36bb4c060c41081fdb531dc17d
Mr-BlackHat-lab <prahalad.hat@outlook.com>
2025-01-18 04:33:06 +0530

    footer done

--- src/Components/footer.jsx
+++ src/Components/footer.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import '../CssOfComponents/footer.css'
 
 function Footer() {
 
 
     return (
-      <>
-      footer
-      </>
+        <div>
+            <footer>
+                <div><p>Copyright &copy;2025 By Prahalad Singh</p></div>
+            </footer>
+        </div>
     )
   }
   
